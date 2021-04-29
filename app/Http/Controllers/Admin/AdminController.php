a17516e355c8051bea3d8194f1b5706ac7e65d03
Dulshan <dulshaniko7@gmail.com>
2021-04-29 16:59:21 +0530

    menu

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,20 +4,25 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+use Symfony\Component\HttpFoundation\Response;
 
 class AdminController extends Controller
 {
    public  function  index(){
-
+       abort_if(Gate::denies('admin'), Response::HTTP_FORBIDDEN, '403 Forbidden');
        return view('admin.index');
    }
     public  function  transfer(){
+        abort_if(Gate::denies('admin'), Response::HTTP_FORBIDDEN, '403 Forbidden');
         return view('admin.transfer');
     }
     public  function  reports(){
+        abort_if(Gate::denies('admin'), Response::HTTP_FORBIDDEN, '403 Forbidden');
         return view('admin.reports');
     }
     public  function  payroll(){
+        abort_if(Gate::denies('admin'), Response::HTTP_FORBIDDEN, '403 Forbidden');
         return view('admin.payroll');
     }
 
